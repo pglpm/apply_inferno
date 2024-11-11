963f9de664e587939561f82ba860a76ac2b1dece
pglpm <pgl@portamana.org>
2024-11-11 13:07:26 +0100

    new calculations

--- nadpark/learn_data.R
+++ nadpark/learn_data.R
@@ -9,7 +9,7 @@ parallel <- 12
 
 ## Name of directory where to save what has been "learned"
 ## a timestamp may be appended to this string
-savedir <- 'output_learn_MDS_PBMC_NAD_iqr2'
+savedir <- 'output_learn_MDS_PBMC_NAD'
 
 ## Call the main function for "learning"
 ## it will save everything in the directory outputdir
@@ -17,8 +17,8 @@ outputdir <- learn(
     data = 'mmc_combine.csv',
     metadata = 'meta_mmc_combine_MDS_PBMC_NAD.csv',
     outputdir = savedir,
-    appendtimestamp = FALSE,
-    appendinfo = FALSE,
+    appendtimestamp = TRUE,
+    appendinfo = TRUE,
     output = 'directory',
     parallel = parallel,
     seed = seed
